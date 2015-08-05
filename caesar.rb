bb66f50676c745bd68f0309a920b3c9051314a41
Lisa Kilker <lisakilker@gmail.com>
2015-08-05 16:05:06 +0200

    updated exercises

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,11 @@
 def solve_cipher(input)
-
+	input_array=input.split("")
+	solution=""
+	input_array.each do |letter|
+		letter=(letter.ord) +1
+		letter=letter.chr
+		solution << letter
   end
- 
-    solve_cipher(input)
+  puts solution
+ end
+    solve_cipher("This is Caesars")
