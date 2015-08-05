16cc93cdf8fd51cf8b0a600f31d693498112311d
Lisa Kilker <lisakilker@gmail.com>
2015-08-05 16:54:26 +0200

    new exercise

---
+++ shoppingcart.rb
@@ -0,0 +1,15 @@
+def add_item(product)
+
+
+
+
+def current_cart
+oranges=Oranges.new(2)
+bananas=Bananas.new(3)
+oranges.price
+bananas.price
+
+my_cart = Cart.new
+my_cart.add_item(oranges)
+my_cart.add_item(bananas)
+my_cart.calculate_total
